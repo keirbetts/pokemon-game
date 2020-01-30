e8086ec18166f9c03af0206d241ea08ed04b825d
Dominic Hui <dominichui112@gmail.com>
2020-01-30 23:02:17 +0000

    added css styling and added correct and incorrect answer state

--- src/components/Pokemoncards.jsx
+++ src/components/Pokemoncards.jsx
@@ -25,7 +25,7 @@ class Pokemoncard extends React.Component {
   };
   render() {
     return (
-      <section>
+      <section className="pokemon-main-container">
         <form onSubmit={this.handleSubmit}>
           <label>
             Guess:
@@ -35,23 +35,29 @@ class Pokemoncard extends React.Component {
         </form>
         {/* {console.log(this.props.randomPokemon)} */}
 
-        <img
-          id={
-            this.props.answer === true ? "pokemon-correct" : "pokemon-incorrect"
-          }
-          src={this.props.randomPokemon.img}
-          alt={this.props.name}
-        />
+        <div className="pokemon-img-container">
+          <img
+            id={
+              this.props.answer === true
+                ? "pokemon-correct"
+                : "pokemon-incorrect"
+            }
+            src={this.props.randomPokemon.img}
+            alt={this.props.name}
+          />
+        </div>
         {this.props.answer === true ? (
           <div className="correct-answer">
-            <h1>CORRECT!</h1>
+            <header className="correct-pokemon-title">
+              {this.props.randomPokemon.name.toUpperCase()}
+            </header>
             <img
               src="https://www.freepngimg.com/thumb/pokemon/37717-4-pokemon-ash-image.png"
               alt="happy-ash"
             />
           </div>
         ) : (
-          <div className="incorrect-answer">
+          <div className="incorrect-answer-container">
             <img
               src="https://camo.githubusercontent.com/8dd9439d771cb25409831294fc728ac61c499b72/68747470733a2f2f692e696d6775722e636f6d2f583962314b75362e706e67"
               alt="who's that pokemon"
